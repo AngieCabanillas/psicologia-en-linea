75d598d3f297d1aba142a818bb3143b8f1b66f58
Aaron Paredes Cabrera <iron0406@outlook.com>
2023-05-04 23:04:02 -0500

    chore: add table academic information to migration

--- backend/src/db/migrations/20230430001341-init-bd.ts
+++ backend/src/db/migrations/20230430001341-init-bd.ts
@@ -3,17 +3,24 @@ import userModel from "../models/user.model";
 import clientModel from "../models/client.model";
 import scheduleModel from "../models/schedule.model";
 import reserveModel from "../models/reserve.model";
+import academicInformationModel from "../models/academic-information.model";
 
 const { UserSchema, USER_TABLE } = userModel;
 const { ClientSchema, CLIENT_TABLE } = clientModel;
 const { ScheduleSchema, SCHEDULE_TABLE } = scheduleModel;
 const { ReserveSchema, RESERVE_TABLE } = reserveModel;
+const { AcademicInformationSchema, ACADEMIC_INFORMATION_TABLE } =
+  academicInformationModel;
 
 export async function up(queryInterface: QueryInterface) {
   await queryInterface.createTable(USER_TABLE, UserSchema);
   await queryInterface.createTable(CLIENT_TABLE, ClientSchema);
   await queryInterface.createTable(SCHEDULE_TABLE, ScheduleSchema);
   await queryInterface.createTable(RESERVE_TABLE, ReserveSchema);
+  await queryInterface.createTable(
+    ACADEMIC_INFORMATION_TABLE,
+    AcademicInformationSchema
+  );
 }
 
 export async function down(queryInterface: QueryInterface) {
@@ -21,4 +28,5 @@ export async function down(queryInterface: QueryInterface) {
   await queryInterface.dropTable(CLIENT_TABLE);
   await queryInterface.dropTable(SCHEDULE_TABLE);
   await queryInterface.dropTable(RESERVE_TABLE);
+  await queryInterface.dropTable(ACADEMIC_INFORMATION_TABLE);
 }
