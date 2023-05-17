e03078a9dcf805fa4720bc3e7f4ac58d759dbef2
Aaron Paredes Cabrera <iron0406@outlook.com>
2023-05-17 00:04:38 -0500

    chore: update reserve

---
+++ frontend/src/shared/services/reserve.service.ts
@@ -0,0 +1,36 @@
+import { ReserveType } from "../types/reserve.type";
+import axiosClient from "../utils/api/clientAxios";
+
+const API = axiosClient.getUri();
+
+const url = `${API}/reserve`;
+
+export const getAllReserves = async () => {
+  return await axiosClient.get(url);
+};
+
+export const getReserveByID = async (id: number) => {
+  return await axiosClient.get(`${url}/${id}`);
+};
+
+export const createReserve = async (reserve: Omit<ReserveType, "id">) => {
+  return await axiosClient.post(url, reserve);
+};
+
+export const editReserve = async (
+  state: Omit<ReserveType, "id">,
+  id: number
+) => {
+  return await axiosClient.put(`${url}/${id}`, state);
+};
+
+export const editStateReserve = async (
+  reserve: { state: string },
+  id: number
+) => {
+  return await axiosClient.put(`${url}/state/${id}`, reserve);
+};
+
+export const deleteReserve = async (id: number) => {
+  return await axiosClient.delete(`${url}/${id}`);
+};
